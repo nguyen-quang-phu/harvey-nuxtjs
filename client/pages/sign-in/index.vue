56031085a4164d0f6ced8a7715f5a467d65552e8
Harvey Nguyen <otis.nguyen.goldenowl@gmail.com>
2021-10-30 18:36:59 +0700

    feat: add singin page

---
+++ client/pages/sign-in/index.vue
@@ -0,0 +1,54 @@
+<template lang="">
+  <v-app>
+    <v-card width="400" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            v-model="email"
+            name="email"
+            label="Email"
+            prepend-icon="mdi-account-circle"
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            name="password"
+            label="Password"
+            :type="showPassword ? 'text' : 'password'"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="info" class="rounded-pill pa-4 ml-auto" @click="signin"
+          >Login</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'App',
+  data() {
+    return {
+      email: '',
+      password: '',
+      showPassword: false,
+    }
+  },
+  methods: {
+    signin(): void {
+      alert('sign in')
+    },
+  },
+})
+</script>
